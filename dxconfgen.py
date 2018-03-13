5a1ac35f7ff16bdfbce758b00aefd0448cfe04c7
Daniel A. Zorin <jfaranime@gmail.com>
2018-03-14 02:18:56 +0300

    Allow multiple blockchain ids in command line
    Signed-off-by: Daniel A. Zorin <jfaranime@gmail.com>

--- dxconfgen.py
+++ dxconfgen.py
@@ -37,7 +37,7 @@ parser.add_argument('--verbose',
     help='verbose flag' )
 
 # Add arguments
-parser.add_argument('-c', '--blockchain', type=str, help='Blockchain config to download', required=True)
+parser.add_argument('-c', '--blockchain', type=str, help='Blockchain config to download', required=True, nargs = '*')
 parser.add_argument('-p2p', '--p2pport', type=str, help='p2pport override', required=False, default=None)
 parser.add_argument('-rpc', '--rpcport', type=str, help='rpcport override', required=False, default=None)
 parser.add_argument('-n', '--configname', type=str, help='config file name', required=False, default=None)
@@ -47,46 +47,54 @@ args = parser.parse_args()
 #print (args)
 
 if args.blockchain:
-  rpcuser = random_gen()
-  rpcpass = random_gen()
-  
-  # find the URL for the chain
-  try:
-    xbridge_text = load_template(chain_lookup(args.blockchain))
-  except urllib.error.HTTPError as e:
-    print("Config for currency {} not found".format(args.blockchain))
-    sys.exit(-1)
-  xbridge_json = json.loads(xbridge_text)
-  xtemplate = Template(xbridge_text)
-  params = {}
-  if args.p2pport:
-    params['p2pPort'] = args.p2pport
-  if args.rpcport:
-    params['rpcPort'] = args.rpcport
-  xresult = xtemplate.render(rpcusername=rpcuser, rpcpassword=rpcpass, **params)
-  xbridge_json = json.loads(xresult)
+  if len(args.blockchain) > 1:
+    if args.p2pport:
+      print("Warning: parameter --p2pport ignored because multiple blockchains were selected.")
+    if args.rpcport:
+      print("Warning: parameter --rpcport ignored because multiple blockchains were selected.")
+    if args.configname:
+      print("Warning: parameter --configname ignored because multiple blockchains were selected.")
+    args.p2pport = args.rpcport = args.configname = None
+  for blockchain in args.blockchain:
+    rpcuser = random_gen()
+    rpcpass = random_gen()
+    
+    # find the URL for the chain
+    try:
+      xbridge_text = load_template(chain_lookup(blockchain))
+    except urllib.error.HTTPError as e:
+      print("Config for currency {} not found".format(blockchain))
+      continue
+    xbridge_json = json.loads(xbridge_text)
+    xtemplate = Template(xbridge_text)
+    params = {}
+    if args.p2pport:
+      params['p2pPort'] = args.p2pport
+    if args.rpcport:
+      params['rpcPort'] = args.rpcport
+    xresult = xtemplate.render(rpcusername=rpcuser, rpcpassword=rpcpass, **params)
+    xbridge_json = json.loads(xresult)
 
-  confFile = list(xbridge_json.values())[0]['Title'].lower()
-  if args.configname:
-    confFile = args.configname.lower()
-  
-  # generate wallet config
-  for x in xbridge_json: p2pport = (xbridge_json[x]['p2pPort'])
-  for x in xbridge_json: rpcport = (xbridge_json[x]['rpcPort']) 
-  res_conf = load_template(walletconfj2_url)  
-  template = Template(res_conf)
-  result = template.render(rpcusername=rpcuser, rpcpassword=rpcpass, p2pPort=p2pport, rpcPort=rpcport)
-  save_config(result, '%s.conf' % confFile)
-  
-  # generate xbridge config
-  xbridge_config = load_template(xbridgeconfj2_url)
-  #f = open("xbridge.conf.j2", "r")
-  #xbridge_config = f.read()
-  xbridge_template = Template(xbridge_config)
-  xbridge_result = xbridge_template.render(blockchain=args.blockchain, val=list(xbridge_json.values())[0])
-  save_config(xbridge_result, confFile+'-xbridge.conf')
-  
-  print(xbridge_result)
-  print ('---')
-  print (result)
-  
+    confFile = list(xbridge_json.values())[0]['Title'].lower()
+    if args.configname:
+      confFile = args.configname.lower()
+    
+    # generate wallet config
+    for x in xbridge_json: p2pport = (xbridge_json[x]['p2pPort'])
+    for x in xbridge_json: rpcport = (xbridge_json[x]['rpcPort']) 
+    res_conf = load_template(walletconfj2_url)  
+    template = Template(res_conf)
+    result = template.render(rpcusername=rpcuser, rpcpassword=rpcpass, p2pPort=p2pport, rpcPort=rpcport)
+    save_config(result, '%s.conf' % confFile)
+      
+    # generate xbridge config
+    xbridge_config = load_template(xbridgeconfj2_url)
+    #f = open("xbridge.conf.j2", "r")
+    #xbridge_config = f.read()
+    xbridge_template = Template(xbridge_config)
+    xbridge_result = xbridge_template.render(blockchain=blockchain, val=list(xbridge_json.values())[0])
+    save_config(xbridge_result, confFile+'-xbridge.conf')
+      
+    print(xbridge_result)
+    print ('---')
+    print (result)
